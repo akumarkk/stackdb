14b133c8ab7a6985c8ab274574454d600ce23058
Anil Kumar Konasale Krishna <akumarkk@outlook.com>
2015-09-08 15:34:43 -0600

    Stackdb dir

--- stackdb_install.sh
+++ stackdb_install.sh
@@ -1,19 +1,25 @@
-sudo apt-get --assume-yes install pkg-config libusb-dev
+sudo apt-get --assume-yes install pkg-config libusb-dev m4
+sudo apt-get --assume-yes install pkg-config libusb-dev 
 sudo apt-get --assume-yes install zlib1g-dev libncurses5-dev
 sudo apt-get --assume-yes install libffi-dev
 sudo apt-get --assume-yes install libbz2-dev
 
-
+STACKDB_DIR=$1
+if[ $# eq 0 ]
+    then
+        echo "Please stackdb base directory!!!";
+	exit -1;
+fi
 
 wget http://ftp.acc.umu.se/pub/gnome/sources/glib/2.45/glib-2.45.7.tar.xz
 tar xvf glib-2.45.7.tar.xz
 cd glib-2.45.7
 ./configure --prefix=/opt/vmi/glib
 make && make install
-cd ~/stackdb
 
 
 #
+cd STACKDB_DIR
 wget https://fedorahosted.org/releases/e/l/elfutils/0.163/elfutils-0.163.tar.bz2 --no-check-certificate
 tar xvfj elfutils-0.163.tar.bz2
 cd elfutils-0.163
