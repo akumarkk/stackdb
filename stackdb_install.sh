170ade5c397bc234281a99ab72c76398b747512f
root <root@stackdbquery.stackdbquery.tcloud.emulab.net>
2015-10-05 21:01:06 -0600

    unzip

--- stackdb_install.sh
+++ stackdb_install.sh
@@ -2,7 +2,7 @@
 install_utils()
 {
 	sudo apt-get --assume-yes update upgrade
-	sudo apt-get --assume-yes install byacc flex bison libssl-dev default-jre default-jdk
+	sudo apt-get --assume-yes install byacc flex bison libssl-dev default-jre default-jdk unzip
 	sudo apt-get --assume-yes install pkg-config libusb-dev m4 autoconf libtool libtool-bin
 	sudo apt-get --assume-yes install pkg-config libusb-dev libglib2.0-dev
 	sudo apt-get --assume-yes install zlib1g-dev libncurses5-dev
